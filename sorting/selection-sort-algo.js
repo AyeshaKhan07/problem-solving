77dea89e1b8902eda0e2eb0ebfa7d227ca5f9593
AyeshaKhan07 <ayeshanadeem9085@gmail.com>
2025-01-31 10:13:14 +0500

    Implementation of selection sort

---
+++ sorting/selection-sort-algo.js
@@ -0,0 +1,25 @@
+/**
+ * Selection sort is a comparison based algorithm in which we iterate over the array to find the 
+ * smallest element and then place it at the first index then we iterate over the next elements 
+ * and find the second smallest and place it on the second index. The process continues until the 
+ * array gets sorted.
+ */
+function sort(array) {
+    for (let i = 0; i < array.length; i++) {
+        let smallestValue = array[i], smallestIndex = i
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[j] < smallestValue) {
+                smallestValue = array[j]
+                smallestIndex = j
+            }
+        }
+
+        const temp = array[i];
+        array[i] = smallestValue;
+        array[smallestIndex] = temp
+    }
+
+    return array
+}
+
+console.log(sort([64, 25, 12, 22, 11, 12]))
