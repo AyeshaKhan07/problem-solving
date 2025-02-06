2e9ee2b300bac5a17f898370bd3b292505abd1b6
AyeshaKhan07 <ayeshanadeem9085@gmail.com>
2025-02-06 11:16:39 +0500

    added description of insertion sort

--- sorting/insertion-sort-algo.js
+++ sorting/insertion-sort-algo.js
@@ -1,5 +1,11 @@
 /**
- * 
+ * As the name suggests, in this sorting algorithm, we take the element and insert it into 
+ * its correct position. We start with the first element, that’s considered sorted of course.
+ * Then we move to the second element and compare it with the first element. We swap it if 
+ * the second element is lesser than the first one else we move to the third element. The 
+ * third element is then compared with the first and second element, and will get swapped 
+ * to its correct position.
+ * further reading: https://docs.google.com/document/d/1HLFlERINHQ9FlAMYYLvfa2_vHdrKUc6xoxYr-fu5zXQ/edit?tab=t.0#heading=h.h8e0ovmhvurg
  */
 function sort(array) {
     for (let i = 1; i < array.length; i++) {
