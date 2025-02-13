e74717e688ff38effacba5a11581da17d777f5cc
AyeshaKhan07 <ayeshanadeem9085@gmail.com>
2025-02-13 15:05:13 +0500

    added description of merge sort

--- sorting/merge-sort-algo.js
+++ sorting/merge-sort-algo.js
@@ -1,3 +1,13 @@
+/**
+ * We recursively divide the unsorted array into sub arrays until the sub array contains only one element.
+ * We start by dividing the array into two halves: left and right, then further divide the left array into
+ * two halves again: left and right, similarly divide the right sub array into another two halves of left 
+ * and right. We repeat the process until the left subarray and the right subarray contain only one element.
+ * Then we start merging the subarrays in sorted order until the merged array returns the sorted list.
+ * Merge sort follows the divide & conquer rule. The idea behind merge sort is: it’s easy to sort smaller 
+ * arrays than the bigger ones.
+ * further reading: https://docs.google.com/document/d/1HLFlERINHQ9FlAMYYLvfa2_vHdrKUc6xoxYr-fu5zXQ/edit?tab=t.0#heading=h.10zb8c68ag2f
+ */
 function divideAndConquer(unsortedArray) {
     const length = unsortedArray.length
     if (length > 1) {
