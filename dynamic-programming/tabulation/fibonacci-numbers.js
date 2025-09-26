1a41d5f4a7e43d21effdd111aafac5c3c277bd97
AyeshaNadeem <ayeshanadeem9085@gmail.com>
2025-09-26 19:05:59 +0500

    implemented fibonacci with optimized space in tabulation approach

--- dynamic-programming/tabulation/fibonacci-numbers.js
+++ dynamic-programming/tabulation/fibonacci-numbers.js
@@ -10,8 +10,8 @@ function getFibonacciNumberAtIndex(n) {
     dp[0] = 0
     dp[1] = 1
 
-    for (let i = 2; i<=n; i++){
-        dp[i] = dp[i-1] + dp[i-2];
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
     }
 
     return dp[n]
@@ -19,3 +19,27 @@ function getFibonacciNumberAtIndex(n) {
 const n = Number(process.argv[2]) || 100
 
 console.log(getFibonacciNumberAtIndex(n))
+
+/**
+ * In the code below, we can see that the current state of 
+ * any fibonacci number depends only on the previous two 
+ * values. So we do not need to store the whole table of 
+ * size n+1 but instead of that we can only store the previous 
+ * two values. 
+ */
+
+function getFibonacciNumberWithOptimizedSpace(n) {
+    let prev1 = 1, prev2 = 0;
+    let curr = 1
+
+    for (let i = 2; i <= n; i++) {
+        curr = prev1 + prev2;
+        
+        prev2 = prev1
+        prev1 = curr
+    }
+
+    return curr
+}
+
+console.log("Fibonacci number with optimized space:",getFibonacciNumberWithOptimizedSpace(n))
