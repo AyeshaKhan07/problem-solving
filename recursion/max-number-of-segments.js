afd85f8a9f1b9d7528ab83b368bcdb15707b152a
AyeshaNadeem <ayeshanadeem9085@gmail.com>
2025-10-08 17:47:05 +0500

    updated implementation of max number of segments with recursion and added time execution log

--- recursion/max-number-of-segments.js
+++ recursion/max-number-of-segments.js
@@ -1,39 +1,17 @@
-// JavaScript implementation to find max cut segments
-// using Recursion
+function maxCutHelper(n, x, y, z, segmentCount = 0) {
+    if (n < 0) return -1
+    if (n == 0) return segmentCount
 
-// Helper function to maximize the number
-// of cuts using recursion
-function maxCutHelper(n, x, y, z) {
+    const cut1 = maxCutHelper(n-x, x, y, z, segmentCount+1)
+    const cut2 = maxCutHelper(n-y, x, y, z, segmentCount+1)
+    const cut3 = maxCutHelper(n-z, x, y, z, segmentCount+1)
 
-    // Base case: If the length is zero, 
-    // return zero cuts
-    if (n === 0) {
-        return 0;
-    }
+    const maxCutCount = Math.max(cut1, cut2, cut3)
 
-    // Base case: If the length becomes negative, 
-    // return an invalid result
-    if (n < 0) {
-        return -1;
-    }
+    if(maxCutCount < 0)
+        return -1
 
-    // Recursive step: Try all three segment 
-    // lengths and choose the maximum result
-    let cut1 = maxCutHelper(n - x, x, y, z);
-    let cut2 = maxCutHelper(n - y, x, y, z);
-    let cut3 = maxCutHelper(n - z, x, y, z);
-
-    // Get the maximum number of cuts among
-    // the 3 options
-    let maxCut = Math.max(cut1, cut2, cut3);
-
-    // If no valid cut found, return negative
-    // value indicating no valid cuts
-    if (maxCut === -1) {
-        return -1;
-    }
-
-    return maxCut + 1;
+    return maxCutCount
 }
 
 // Main function to start the cutting process
@@ -47,9 +25,11 @@ function maximizeTheCuts(n, x, y, z) {
     return res;
 }
 
-let n = 11;
+let n = 25;
 let x = 2;
 let y = 3;
 let z = 5;
 
-console.log(maximizeTheCuts(n, x, y, z));
+console.time("Execution time")
+console.log(maximizeTheCuts(n, x, y, z))
+console.timeEnd("Execution time")
