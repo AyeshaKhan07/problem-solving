48c0328534381447c6d62dabf3cdc66b737c843e
AyeshaNadeem <ayeshanadeem9085@gmail.com>
2026-09-01 11:38:40 +0500

    Add iterative power calculation function and update console output

--- competitve-programming/binary-exponentiation/calculate-number-raise-to-the-power.js
+++ competitve-programming/binary-exponentiation/calculate-number-raise-to-the-power.js
@@ -15,7 +15,29 @@ function calculatePowerByRecursion(number, exponent) {
     else return number * calculatePowerByRecursion(number * number, (exponent - 1) / 2)
 }
 
+function calculatePowerByIteration(number, exponent) {
+    if (exponent == 0)
+        return 1
+    if (exponent == 1)
+        return number
+
+    let n = number, e = exponent
+
+    while (e > 1) {
+        console.log(n, e)
+        n = n * n
+        if (e % 2 == 0)
+            e = e / 2
+
+        else e = (e - 1) / 2
+    }
+
+    return n
+
+}
+
 const number = Number(process.argv[2]) || 10
 const exponent = Number(process.argv[3]) || 3
 
 console.log("By Recusion:", calculatePowerByRecursion(number, exponent))
+console.log("By Iteration:", calculatePowerByIteration(number, exponent))
